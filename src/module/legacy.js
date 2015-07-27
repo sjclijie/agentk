52370b278e528fd4319d525075943d48bf114aff
kyriosli <lz3259@gmail.com>
2015-07-27 18:03:04 +0800

    save

--- src/module/legacy.js
+++ src/module/legacy.js
@@ -34,16 +34,27 @@ const emptyHandler = {
 export function middleware(cb) {
     return function (req) {
         return co.promise(function (resolve, reject) {
-            let res = req.response, $end = res.end;
-            res.end = function () {
-                $end.apply(res, arguments);
-                resolve(emptyHandler); //
-            };
-
+            let res = req.response;
+            initResponse(res);
             cb(req, res, function (err) {
                 if (err) reject(err);
                 else resolve();
             })
         })
+    }
+}
+
+function initResponse(res, resolve) {
+    if (res.hasOwnProperty('_resolvers')) {
+        res._resolvers.push(resolve);
+    } else {
+        const resolvers = res._resolvers = [resolve];
+        const $end = res.end;
+        res.end = function () {
+            $end.apply(res, arguments);
+            for (let i = 0, L = resolvers.length; i < L; i++) {
+                resolvers[i](emptyHandler);
+            }
+        }
     }
 }
