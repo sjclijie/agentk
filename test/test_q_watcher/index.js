08bf3978f43621b5fcf366fa37829afb2a54e337
kyriosli <lz3259@gmail.com>
2015-08-12 16:00:04 +0800

    save

--- test/test_q_watcher/index.js
+++ test/test_q_watcher/index.js
@@ -2,8 +2,6 @@ import * as q_watcher from '../../src/module/q_watcher.js';
 
 q_watcher.prefix = 't.agentk.q_watcher';
 
-q_watcher.setupPeers(['l-qzz1.fe.dev.cn6', 'l-qzz2.fe.dev.cn6'], require('os').hostname(), 8012);
-
 setTimeout(function func() {
     let t = Math.random() * 6000;
     q_watcher.add('t_' + (Math.random() * 5 | 0), t);
