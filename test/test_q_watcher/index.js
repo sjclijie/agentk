6005bf9b9b89270e1da324e01e813d1bf7217001
kyriosli <lz3259@gmail.com>
2015-08-12 14:36:44 +0800

    save

---
+++ test/test_q_watcher/index.js
@@ -0,0 +1,11 @@
+import * as q_watcher from '../../src/module/q_watcher.js';
+
+q_watcher.prefix = 't.agentk.q_watcher';
+
+q_watcher.setupPeers(['l-qzz1.fe.dev.cn6', 'l-qzz2.fe.dev.cn6'], require('os').hostname(), 8012);
+
+setTimeout(function func() {
+    let t = Math.random() * 6000;
+    q_watcher.recordOne('t_' + (Math.random() * 5 | 0), t);
+    setTimeout(func, t);
+}, 1000);
