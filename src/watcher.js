3e1ced66ce1c6374af2fdf3b3b07d23432959e72
kyriosli <lz3259@gmail.com>
2015-10-27 13:17:29 +0800

    patch recursive watch for linux

--- src/watcher.js
+++ src/watcher.js
@@ -4,13 +4,27 @@ const cp = require('child_process'),
     fs = require('fs'),
     path = require('path');
 
+const watch = process.platform === 'linux' ? function (dir, cb) {
+    fs.watch(dir, cb);
+
+    for (let name of fs.readdirSync(dir)) {
+        let subpath = path.join(dir, name);
+        if (fs.statSync(subpath).isDirectory()) {
+            watch(subpath, cb);
+        }
+    }
+} : function (dir, cb) {
+    fs.watch(dir, {recursive: true}, cb);
+};
+
 exports.run = function (dir) {
     let srcDir = path.resolve(dir, 'src');
     console.log('watching ' + srcDir);
 
     let worker, respawning, lastRespawn = Date.now();
     spawn();
-    fs.watch(srcDir, {recursive: true}, function onevent() {
+
+    watch(srcDir, function onevent() {
         if (respawning) return;
         respawning = true;
         if (Date.now() - lastRespawn < 1000) {
