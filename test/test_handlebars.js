46a89ced18ffdc3d7966baa50a74b3da6b059451
kyriosli <lz3259@gmail.com>
2015-11-24 10:21:52 +0800

    add test suits

--- test/test_handlebars.js
+++ test/test_handlebars.js
@@ -1,8 +1,8 @@
 import * as view from '../src/module/view';
 
-import * as Handlebars from '../src/module/handlebars';
+import Handlebars from '../src/module/handlebars';
 
-let h = view.engine(Handlebars.compile);
+let h = view.engine(Handlebars.fast_compile);
 
 console.log(h('test/extra/test.handlebars', [{name: 'John'}]) + '');
 console.log(h('test/extra/test.handlebars', [{name: 'Jack. 李'}]) + '');
